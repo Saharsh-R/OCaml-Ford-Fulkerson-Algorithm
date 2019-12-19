3ef99ec5945463a78e4c864c0a149e994b3ec6fe
Saharsh-R <48884865+Saharsh-R@users.noreply.github.com>
2019-12-19 11:33:21 +0100

    Update algo.ml

--- src/algo.ml
+++ src/algo.ml
@@ -1,7 +1,8 @@
 open Graph 
 open Tools
+type flow = int
 
-let rec neighbour_sub l = match l with
+let rec neighbour_sub l = match l with   
   |(id,x)::rest -> id::neighbour_sub rest 
   |[] -> []
 
@@ -19,8 +20,8 @@ let unvisited_neighbour graph node visited_list = unvisited_neighbour_sub visite
 
 let add_visited visited_list element_list = List.append element_list visited_list 
 
-type flow = int 
-
+ 
+(* Find out flow for a given path *)
 let rec var_flow_sub graph path flow = 
   match path with 
   |a::b::rest ->( 
@@ -36,6 +37,7 @@ let rec var_flow_sub graph path flow =
 
 let var_flow graph path = var_flow_sub graph path (-1);;
 
+
 let rec visit_course graph current visited_list target = 
   if(current = target && ((List.mem target visited_list) = false)) then (add_visited visited_list [target],  [target])
   else 
@@ -43,29 +45,37 @@ let rec visit_course graph current visited_list target =
     if(neighbour1 = []) 
     then (add_visited visited_list [current],  [])
     else 
-      let child_res = (List.fold_left (fun (rand,pth) x -> match (visit_course graph x rand target) with |(randa,pthr) -> (randa, add_visited pthr pth)) (add_visited visited_list [current], []) neighbour1 ) in 
+      let child_res = 
+      (List.fold_left 
+        (fun (rand,pth) x -> match (visit_course graph x rand target) with 
+          |(randa,pthr) -> (randa, add_visited pthr pth)) 
+        (add_visited visited_list [current], []) neighbour1 ) in 
       match (child_res) with
       | (x,[]) -> ((current::x),[])
       | (x,y) -> ((current::x), (current::y))
 
-let rec continue_path node1 node2 path_list = 
-  match path_list  with
+(* Find if node1 and node2 exist in path *)
+let rec continue_path node1 node2 path = 
+  match path with
   | a::b::rest -> if ((a=node1) && (b=node2)) then true else (false || (continue_path node1 node2 (b::rest)))
   | a::[] -> false
   | [] -> false
 
+(* Label is current capacity between node1 and node2, flow is the flow value we're trying to send from node1 to node 2 *)
+(* Updates edge weight between the two nodes *)
 let update_graph_sub graph node1 node2 label path_list flow= 
-  if (continue_path  node1 node2 path_list) 
+  if (continue_path  node1 node2 path_list) (*If node1 and node2 edge exist in path*)
   then 
-    if((label-flow)=0) then (add_arc graph node2 node1 label) 
+    if((label-flow)=0) then (add_arc graph node2 node1 label) (*if the edge is full*)
     else (add_arc  (add_arc graph node2 node1 (flow)) node1 node2 (label-flow))
 
-  else (add_arc graph node1 node2 label)
+  else (add_arc graph node1 node2 label) 
 
+(* Updates the entire graph along the path*)
 let update_graph graph path_list flow = 
   let res_graph = clone_nodes graph in
-  let nv_add_arc a b c d = (update_graph_sub a b c d path_list flow) in 
-  e_fold graph nv_add_arc res_graph;;
+  let new_add_arc a b c d = (update_graph_sub a b c d path_list flow) in 
+  e_fold graph new_add_arc res_graph;;
 
 let rec loop_ff (graph,flow,start,ends) = 
   match (visit_course graph start [] ends) with
@@ -74,7 +84,7 @@ let rec loop_ff (graph,flow,start,ends) =
     then (graph,flow,start,ends)
     else (
       let vari_flow = (var_flow graph y) in 
-      let nv_graph = (update_graph graph y vari_flow) in
+      let new_graph = (update_graph graph y vari_flow) in
 
-      loop_ff (nv_graph,(flow + vari_flow),start,ends)
+      loop_ff (new_graph,(flow + vari_flow),start,ends)
     )
